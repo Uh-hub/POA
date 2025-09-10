5f9733e97c76ca72b44afa0008e3d38c63ac977d
Noa <tabacal3590@gmail.com>
2025-09-10 11:17:53 +0900

    [ 🔨script ] 언리얼 버전 5.6 -> 5.5 로 변경

--- Source/POAEditor.Target.cs
+++ Source/POAEditor.Target.cs
@@ -9,7 +9,7 @@ public class POAEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
 		ExtraModuleNames.Add("POA");
 	}
 }
