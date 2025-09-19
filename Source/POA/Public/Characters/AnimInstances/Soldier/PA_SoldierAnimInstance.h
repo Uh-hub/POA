7d977f72a79257a3a59d98f4e5c64bf2ee576ee1
Noa <tabacal3590@gmail.com>
2025-09-19 21:24:06 +0900

    [ ✨ update ]  플레이어의 Relax state 추가
    + idle 상태에서 일정 시간 움직임이 없으면 Relax state 애니메이션을 랜덤으로 재생함

--- Source/POA/Public/Characters/AnimInstances/Soldier/PA_SoldierAnimInstance.h
+++ Source/POA/Public/Characters/AnimInstances/Soldier/PA_SoldierAnimInstance.h
@@ -4,10 +4,27 @@
 #include "Characters/AnimInstances/PA_CharacterAnimInstance.h"
 #include "PA_SoldierAnimInstance.generated.h"
 
-
+class APA_Soldier;
 UCLASS()
 class POA_API UPA_SoldierAnimInstance : public UPA_CharacterAnimInstance
 {
 	GENERATED_BODY()
 	
+public:
+
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|References")
+	APA_Soldier* OwningSoldier;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	bool bShouldEnterRelaxState;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	float EnterRelaxStateThreshold = 5.f;
+
+
+	float IdleElapsedTime;
 };
